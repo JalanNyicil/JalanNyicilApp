be66a0cb829c5dc869666181420523968d95a552
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2018-07-13 11:37:09 +0700

    update event, venue, and attraction with axios

--- server/controllers/ticketmaster.js
+++ server/controllers/ticketmaster.js
@@ -1,16 +1,72 @@
+
 const ticketmaster = require('ticketmaster');
 
-class TicketMaster {
-  static allEvent (req, res) {
-    ticketmaster('UJoSGON0AX0CleYYdOyGNRY4rDmKpuSF').discovery.v2.event.all()
-    .then( (result) => {
-    
-      res
-        .status(200)
-        .json(result)
-      // "result" is an object of Ticketmaster events information
-    });
+const axios = require('axios')
 
+
+
+class TicketMaster {
+  static allEvent(req, res) {
+    console.log(process.env.TM_KEY)
+    axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${process.env.TM_KEY}`)
+      .then(response => {
+        res
+          .status(200)
+          .json(response.data)
+      })
+
+      .catch(err => {
+        res
+          .status(401)
+          .json(err)
+      })
+  }
+
+  static allVenue(req, res) {
+    axios.get(`https://app.ticketmaster.com/discovery/v2/venues.json?apikey=${process.env.TM_KEY}`)
+      .then(response => {
+        // console.log(response)
+        res
+          .status(200)
+          .json(response.data)
+      })
+
+      .catch(err => {
+        res
+          .status(401)
+          .json(err)
+      })
+  }
+
+  static allAttractions(req, res) {
+    axios.get(`https://app.ticketmaster.com/discovery/v2/attractions.json?apikey=${process.env.TM_KEY}`)
+      .then(response => {
+        // console.log(response)
+        res
+          .status(200)
+          .json(response.data)
+      })
+
+      .catch(err => {
+        res
+          .status(401)
+          .json(err)
+      })
+  }
+
+  static findEvent(req, res) {
+    axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${process.env.TM_KEY}`)
+      .then(response => {
+        res
+          .status(200)
+          .json(response.data)
+      })
+
+      .catch(err => {
+        res
+          .status(401)
+          .json(err)
+      })
   }
 }
 
--- server/routes/ticketmaster.js
+++ server/routes/ticketmaster.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const ticketMasterController = require('../controllers/ticketmaster')
 
-router.get('/', ticketMasterController.allEvent)
+router.get('/event', ticketMasterController.allEvent)
+router.get('/venue', ticketMasterController.allVenue)
+router.get('/attraction', ticketMasterController.allAttractions)
+router.get('/findevent', ticketMasterController.findEvent)
 
 module.exports = router
