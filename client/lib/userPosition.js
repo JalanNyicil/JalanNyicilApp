dfa98703eee48bc9f04ff612bfc80f6e985b904d
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-07-13 09:58:44 +0700

    renamed eventsOnAMap to userPosition

---
+++ client/lib/userPosition.js
@@ -0,0 +1,110 @@
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+    var x = document.getElementById("location");
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  var x = document.getElementById("location");
+  x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+
+  var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+  initMap(position);
+  // $.ajax({
+  //   type:"GET",
+  //   url:"https://app.ticketmaster.com/discovery/v2/events.json?apikey=U1A7yPBp6jdmOdO7NoDXoG8QK5wwgcPB&latlong="+latlon,
+  //   async:true,
+  //   dataType: "json",
+  //   success: function(json) {
+  //               console.log(json);
+  //               var e = document.getElementById("events");
+  //               e.innerHTML = json.page.totalElements + " events found.";
+  //               // showEvents(json);
+  //               initMap(position, json);
+  //            },
+  //   error: function(xhr, status, err) {
+  //               console.log(err);
+  //            }
+  // });
+
+}
+
+function showError(error) {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      x.innerHTML = "User denied the request for Geolocation."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML = "Location information is unavailable."
+      break;
+    case error.TIMEOUT:
+      x.innerHTML = "The request to get user location timed out."
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML = "An unknown error occurred."
+      break;
+  }
+}
+
+
+// function showEvents(json) {
+//   for(var i=0; i<json.page.size; i++) {
+//     $("#events").append("<p>"+json._embedded.events[i].name+"</p>");
+//   }
+// }
+
+
+function initMap(position) {
+  var mapDiv = document.getElementById('map');
+  var map = new google.maps.Map(mapDiv, {
+    center: {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    },
+    zoom: 10
+  });
+  marker = new google.maps.Marker({
+    position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+    map: map,
+    draggable: true,
+    title: "Drag me!",
+  });
+
+  marker.addListener('click', toggleBounce);
+
+  google.maps.event.addListener(marker, 'dragend', function(evt) {
+    document.getElementById('location').innerHTML = 'Latitude: ' + evt.latLng.lat().toFixed(3) + '<br>Longitude: ' + evt.latLng.lng().toFixed(3);
+  });
+
+  google.maps.event.addListener(marker, 'dragstart', function(evt) {
+    document.getElementById('location').innerHTML = 'Latitude: ' + evt.latLng.lat().toFixed(3) + '<br>Longitude: ' + evt.latLng.lng().toFixed(3);
+  });
+
+  marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+  console.log(marker);
+  // for(var i=0; i<json.page.size; i++) {
+  //   addMarker(map, json._embedded.events[i]);
+  // }
+}
+
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+// function addMarker(map, event) {
+//   var marker = new google.maps.Marker({
+//     position: new google.maps.LatLng(event._embedded.venues[0].location.latitude, event._embedded.venues[0].location.longitude),
+//     map: map
+//   });
+//   marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+//   console.log(marker);
+// }
+
+getLocation();
